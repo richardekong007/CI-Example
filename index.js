e6d20583be73acdf46a5863eeed0f78f7abe5278
Richard Ekong <ekong.richard@yahoo.com>
2019-03-10 14:17:55 +0200

    Modify index.js

---
+++ index.js
@@ -0,0 +1,21 @@
+function square(a){
+        return a*a;
+}
+
+function squareRoot(a){
+        return a**(1/2);
+}
+
+function cubeOf (a){
+        return a ** 3;
+}
+
+function cubeRoot(a){
+        return a**(1/3);
+}
+
+function quadOf(a){
+        return a**4;
+}
+
+
